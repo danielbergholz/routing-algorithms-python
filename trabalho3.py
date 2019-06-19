726b02b68098e9fb767debbfe4fe6a6506e1a745
gobbidaniel <bergholz.daniel@gmail.com>
2019-06-19 18:48:42 -0300

    Funcao ler_dot() implementada

--- trabalho3.py
+++ trabalho3.py
@@ -4,7 +4,7 @@
 # TRABALHO 3 FUNDAMENTOS DE REDES 1
 
 from igraph import *
-import os
+import os, re
 
 # variavel global para ir armazenando decisoes do usuario 
 n = 0
@@ -39,14 +39,76 @@ def menu(primeira_vez=True):
     while (n < 1 or n > 5):
         n = input('Escolha um numero valido (entre 1 e 5):\n')
 
+def ler_dot():
+    lista = []
+    vertices = []
+    vertices_aux = []
+    n_vertices = 0
+
+    # abrir arquivo do usuario e ler cada linha 
+    x = raw_input('Por favor digite o nome do arquivo: (eh preciso ter .dot no final e o PATH adequado)\n')
+    file = open(x, 'r')
+    linhas = file.readlines()
+    pattern = '\w+\s*'
+
+    # lista = variavel que guarda par de vertices que possuem aresta
+    # comeca a ler da linha 1 ao inves da 0, pois a linha zero contem o nome do grafo
+    for i in range(1, len(linhas)): 
+        lista.append(re.findall(pattern, linhas[i]))
+
+    # lista = lista de listas. vertices = lista
+    for sublista in lista:
+        for item in sublista:
+            vertices.append(item)
+
+    # caso haja qualquer item vazio, eliminar da lista
+    # repetir isso ate que nao haja mais item nenhum vazio
+    while True:
+        try:
+            vertices.remove('')
+        except ValueError:
+            break 
+
+    # loop para eliminar duplicatas na lista "vertices"
+    for i in vertices:
+        if i not in vertices_aux:
+            vertices_aux.append(i)
+    vertices = vertices_aux
+
+    # finalmente, vertices se tornou uma lista "limpa"
+    n_vertices = len(vertices)
+    g.add_vertices(n_vertices)
+    g.vs["name"] = vertices
+
+    # agora iremos adicionar as arestas no grafo
+    tupla = ()
+    arestas = []
+    for i in range(len(lista)):
+        if (len(sublista) == 2):
+            for sublista in lista[i]:
+                tupla = (sublista[0], sublista[1])
+                arestas.append(tupla) 
+    g.add_edges(arestas)
+    g.es["weight"] = [0]*len(vertices)
+
+
+    file.close()
+    
+
 def criar_grafo():
     print 'Agora iremos criar seu grafo de testes\n'
     y = raw_input('Voce gostaria de carregar um grafo a partir de um arquivo .DOT? (s/n)\n')
     if (y == 's'):
-        print 'kk eae men'
+        ler_dot()
         # fazer algo aqui depois
     else:
-        x = input('Quantos vertices tem seu grafo?\n')
+        while True:
+            try:
+                x = input('Quantos vertices tem seu grafo?\n')
+            except SyntaxError:
+                print 'Voce somente apertou ENTER. Por favor, digite um numero valido\n'
+                continue
+            break
         g.add_vertices(x)
         lista = [] # lista auxiliar
         for i in range(g.vcount()):
@@ -91,3 +153,12 @@ def main():
 
 if __name__ == "__main__":
     main()
+
+
+
+
+
+
+
+
+
